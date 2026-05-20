bc8759eea6623745109823bf7fe1dbe13fa8a12d
Hernandes de Castro Guereta <132833897+HernandesGuereta@users.noreply.github.com>
2026-05-20 15:46:57 +0000

    Navbar create

---
+++ projeto-vitalis/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+import Menu from './components/Menu'
+
+function App() {
+  return(
+    <div>
+      <Menu />
+    </div>
+  )
+}
+export default App
---
+++ projeto-vitalis/src/components/Menu.jsx
@@ -0,0 +1,17 @@
+
+function Menu() {
+    return (
+       <nav>
+        <div className="logo">
+            <span className="nome">Vitalis</span>
+            <span className="subtitulo">Hemocentro</span>
+        </div>
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/gestao-estoque">Gestão de Estoque</a></li>
+        </ul>
+       </nav>
+    )
+}
+
+export default Menu
